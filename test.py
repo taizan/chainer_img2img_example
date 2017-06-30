74840da0608ef69ce1792996bbf87de547ec3e8a
taizan <you@example.com>
2017-06-30 17:44:16 +0900

    fix bug

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ import chainer.links as L
 import chainer.datasets.image_dataset as ImageDataset
 import six
 import os
+import cv2
 
 from chainer import cuda, optimizers, serializers, Variable
 from chainer import training
@@ -42,17 +43,17 @@ def main():
         chainer.cuda.get_device(args.gpu).use()  # Make a specified GPU current
 
     cnn = net.AutoENC()
-    serializers.load_npz(args.model, cnn)i
+    serializers.load_npz(args.model, cnn)
 
     if args.gpu >= 0:
         cnn.to_gpu()  # Copy the model to the GPU
 
     dataset = Image2ImageDataset(args.filelist)
     src , dst  =  dataset.get_example(0)
-    x_in = np.zeros((batchsize, src.shape[1], src.shape[1], src.shape[2])).astype('f')
+    x_in = np.zeros((args.batchsize, src.shape[0], src.shape[1], src.shape[2])).astype('f')
     #x_out = np.zeros((batchsize, dst.shape[1], dst.shape[1], dst.shape[2])).astype('f')
 
-    for j in range(batchsize):
+    for j in range(args.batchsize):
         src , dst  =  dataset.get_example(j)
         x_in[j,:] =  src
         #x_out[j,:] =  dst
@@ -63,7 +64,7 @@ def main():
     x_out = cnn(x_in)
     output = x_out.data.get()
 
-    for i in range(batchsize):
+    for i in range(args.batchsize):
         img = dataset.post_proc(output[i])
         cv2.imwrite( args.out +"/"+ dataset.get_name(i) , img )
 
